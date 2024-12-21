6b9b07f179ef9d1d246fafe4b37d501e9089a945
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-21 23:07:51 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import HomeMarquee from "./components/HomeMarquee";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="bg-black text-primary p-2">
-      {/* wrapper  */}
-      <div className="bg-secondary rounded-[1.5rem] min-h-screen">
-        <Navbar />
-        <Hero />
-        <HomeMarquee />
+    <Router>
+      <div className="bg-black text-primary p-2">
+        {/* wrapper  */}
+        <div className="bg-secondary rounded-[1.5rem] min-h-screen">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import HomeMarquee from "../components/HomeMarquee";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <HomeMarquee />
+    </div>
+  );
+};
+
+export default Home;
