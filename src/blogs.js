c9bf5758d31cedab5f41ed2d3f1cffb7662e6b9a
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-22 11:26:03 +0530

    json created for blogs

---
+++ src/blogs.js
@@ -0,0 +1,43 @@
+export const blogs = [
+  {
+    id: 1,
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*IAl6z-PtCG2Wrww0sHMvfg.png",
+    date: "May 28, 2024",
+    title: "Redux Toolkit Made Easy: State Management in React",
+    link: "https://medium.com/@vaishnavi_shelke/redux-toolkit-made-easy-state-management-in-react-3c07c182af17",
+  },
+  {
+    id: 2,
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*Luu2y5196Aq_kVSJGZIGxA.jpeg",
+    date: "June 21, 2024",
+    title: "How Networking Works: From Click to Web Page",
+    link: "https://medium.com/@vaishnavi_shelke/how-networking-works-from-click-to-web-page-b9fa1663d134",
+  },
+  {
+    id: 3,
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*4PBnkSh6KPn72kKuLjJ88w.jpeg",
+    date: "June 26, 2024",
+    title: "A Guide for Building a Server Using  Express.js",
+    link: "https://medium.com/@vaishnavi_shelke/building-a-server-using-express-js-2eff4ac6f14f",
+  },
+  {
+    id: 4,
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*MOzvLUDGTKBDgVKxYiD1Xw.jpeg",
+    date: "June 25, 2024",
+    title: "A Comprehensive Guide to React Router Hooks",
+    link: "https://medium.com/@vaishnavi_shelke/a-guide-to-react-router-hooks-94770b9948ac",
+  },
+  //   {
+  //     id: 5,
+  //     image:
+  //       "https://miro.medium.com/v2/resize:fit:828/format:webp/1*caNXUcq-94tt0WbBJQawqw.png",
+  //     date: "May 31, 2024",
+  //     title:
+  //       "GitHub Collaboration Guide: A Beginner’s Guide to Collaborative Coding",
+  //     link: "https://medium.com/@vaishnavi_shelke/github-collaboration-guide-acaab67166fc",
+  //   },
+];
