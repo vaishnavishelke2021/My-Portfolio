76a8437753ed46757f57e2d371062f251a87862f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-22 09:07:00 +0530

    json created for projects

---
+++ src/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    id: 1,
+    img: "https://framerusercontent.com/images/AGpBGSlAeiBe8absF2xAK8RBIA.jpeg?scale-down-to=512",
+    link: "/projects/uranium-tracker",
+    title: "Internship",
+    category: "Web Pages",
+    technologies: ["React", "Tailwind CSS", "JavaScript", "GSAP"],
+    duration: "4 months",
+    heading: "Intership Project: Uranium Tracker",
+    description:
+      "A web page showcasing the projects completed during my internship, including responsive design and interactive features.",
+    moreImages: [
+      "https://framerusercontent.com/images/sample1.jpeg",
+      "https://framerusercontent.com/images/sample2.jpeg",
+    ],
+    liveLink: "https://example.com/internship",
+    githubLink: "",
+  },
+  {
+    id: 2,
+    img: "https://framerusercontent.com/images/AGpBGSlAeiBe8absF2xAK8RBIA.jpeg?scale-down-to=512",
+    link: "/projects/byteup-ai",
+    title: "Internship",
+    category: "Web Pages",
+    technologies: ["React", "tailwind CSS", "GSAP"],
+    duration: "7 days",
+    heading: "Internship Project: Company Website",
+    description:
+      "A personal UI design project that focuses on modern aesthetics and animations.",
+    moreImages: [
+      "https://framerusercontent.com/images/sample3.jpeg",
+      "https://framerusercontent.com/images/sample4.jpeg",
+    ],
+    liveLink: "https://example.com/personal",
+    githubLink: "https://github.com/username/personal-project",
+  },
+];
