f75343d7205192ce1ae373eebf6d54cd1e0995ef
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-22 10:52:04 +0530

    json created for experience

---
+++ src/experience.js
@@ -0,0 +1,37 @@
+export const experiences = [
+  {
+    id: 1,
+    date: "Aug 2024 - Present",
+    company: "ByteUp AI",
+    role: "Frontend Developer",
+    responsibilities: [
+      "Currently working on developing a AI based product website",
+      "Developed client websites using React,Tailwind CSS, HTML, CSS, JavaScript, and GSAP for real-world projects",
+      "Built the company s website using React.js, ensuring responsive design and smooth functionality",
+      "Conducted research and designed user interfaces for websites to ensure optimal user experience",
+      "Collaborated with fellow interns and contributed to project repositories on GitHub",
+    ],
+  },
+  {
+    id: 2,
+    date: "Feb 2022 - Jun 2024",
+    company: "STSARC Private Limited",
+    role: "Co-founder & UI/UX Designer",
+    responsibilities: [
+      "Developed the company's website using React & TailwindCSS ensuring a responsive design",
+      "Designed user interfaces (UIs) for client projects in the AI/ML and product sectors, focusing on usability and aesthetics",
+      "Created wireframes and prototypes to iterate on design concepts and gather user feedback",
+      "Responsibe for designing mobile app UI using figma for all products",
+    ],
+  },
+  {
+    id: 3,
+    date: "Jul 2023 - Aug 2023",
+    company: "Academor",
+    role: "Web Developer",
+    responsibilities: [
+      "Responsibe for building projects using HTML, Tailwind CSS and JavaScript",
+      "Collaborated with fellow interns and contributed to project repositories on GitHub",
+    ],
+  },
+];
