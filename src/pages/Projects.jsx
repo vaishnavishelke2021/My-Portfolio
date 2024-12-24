b68d195b1cfad5e7771342a3a3849451e078da5e
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-24 10:07:05 +0530

    Projects heading added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import ProjectsFilter from "../components/ProjectsFilter";
 
 const Projects = () => {
   return (
-    <div>
-      Projects
-    </div>
-  )
-}
+    <div className="w-full md:w-[90%] mx-auto px-4 md:px-6">
+      {/* heading  */}
+      <div className=" mt-12">
+        <h1 className="text-3xl md:text-[54px] font-medium md:leading-[58px] mb-6 text-primary">
+          My Personal & Internship Projects
+        </h1>
+        <p className="text-lg md:text-xl mb-10">
+          These projects include a variety of tasks and projects I have
+          completed during my internship and personal time. They showcase my
+          skills and experience in web development as well as UI designing.{" "}
+        </p>
+      </div>
 
-export default Projects
+      {/* filter  */}
+      <div className="mt-5">
+        <ProjectsFilter />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
