7e484f1c9d59c93854c4e343e25848c3f000297c
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-12-24 10:03:27 +0530

    Resume Heading added

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import ResumeSec from "../components/ResumeSec";
 
 const Resume = () => {
   return (
     <div>
-      Resume
-    </div>
-  )
-}
+      {/* heading  */}
+      <div className="w-full md:w-[80%] lg:w-[58%] mx-auto px-4 md:px-6 mt-12">
+        <h1 className="text-3xl md:text-[54px] font-medium md:leading-[58px] mb-6 text-primary">
+          Graduation Complete. Code Journey Begins.
+        </h1>
+        <p className="text-lg md:text-xl mb-10">
+          With a passion for frontend development and a drive to build
+          meaningful digital experiences, I’m ready to bring ideas to life
+          through clean code and creative design. Excited to grow, learn, and
+          contribute to projects that make a difference.
+        </p>
+        <a
+          href=""
+          className="bg-primary hover:bg-primary/80 transition-all duration-300 ease-in-out px-5 py-2.5 rounded-lg text-secondary "
+          target="_blank"
+        >
+          Download Resume
+        </a>
+      </div>
 
-export default Resume
+      {/* resume  */}
+      <div className="bg-accent mt-20">
+        <ResumeSec />
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
